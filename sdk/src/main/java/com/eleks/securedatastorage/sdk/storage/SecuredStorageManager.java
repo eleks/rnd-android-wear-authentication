d09a07aa4874ad776c3d8da63da97f537990db55
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-17 18:27:33 +0300

    Added manager class.

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/storage/SecuredStorageManager.java
@@ -0,0 +1,13 @@
+package com.eleks.securedatastorage.sdk.storage;
+
+import android.content.Context;
+
+/**
+ * Created by Serhiy.Krasovskyy on 17.06.2015.
+ */
+public class SecuredStorageManager {
+
+
+    public SecuredStorageManager(Context context) {
+    }
+}
