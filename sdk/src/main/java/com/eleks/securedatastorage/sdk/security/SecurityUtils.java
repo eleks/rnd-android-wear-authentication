e18355b8143452f25447695ff61204f105fb839c
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-17 23:19:06 +0300

    Added class that contains methods for serialize/deserialize objects to bytes array.

--- sdk/src/main/java/com/eleks/securedatastorage/sdk/security/SecurityUtils.java
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/security/SecurityUtils.java
@@ -1,7 +1,29 @@
 package com.eleks.securedatastorage.sdk.security;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Created by Sergey on 17.06.2015.
  */
 public class SecurityUtils {
+
+    public static byte[] serializeObject(Object obj) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(obj);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static Object deserializeObject(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        return objectInputStream.readObject();
+    }
+
+
+
 }
