0eede7ed0c694831a0c6a47840e8883540a28139
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-19 11:54:37 +0300

    Added password dialog

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/dialogs/PasswordDialog.java
@@ -0,0 +1,21 @@
+package com.eleks.securedatastorage.sdk.dialogs;
+
+import android.content.Context;
+
+import com.eleks.securedatastorage.sdk.interfaces.OnGetHalfOfKeyListener;
+
+/**
+ * Created by Serhiy.Krasovskyy on 19.06.2015.
+ */
+public class PasswordDialog {
+
+    private Context mContext;
+
+    public PasswordDialog(Context context) {
+        this.mContext = context;
+    }
+
+    public void show(OnGetHalfOfKeyListener getHalfOfKeyListener) {
+        
+    }
+}
