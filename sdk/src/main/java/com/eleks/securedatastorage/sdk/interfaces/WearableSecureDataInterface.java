c22f49dea684d724f2ec08ef79bdd017d6943096
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-19 11:39:57 +0300

    Renamed and moved interface

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/interfaces/WearableSecureDataInterface.java
@@ -0,0 +1,10 @@
+package com.eleks.securedatastorage.sdk.interfaces;
+
+/**
+ * Created by Serhiy.Krasovskyy on 18.06.2015.
+ */
+public interface WearableSecureDataInterface {
+
+    byte[] getDeviceHalfOfKey();
+    boolean isPairedDeviceConnected();
+}
