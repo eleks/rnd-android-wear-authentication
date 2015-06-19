035c313a7eb5b434109a37cab8a432501e216412
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-19 11:56:10 +0300

    Changed constructor. Added method to get device half of key;

--- sdk/src/main/java/com/eleks/securedatastorage/sdk/wearable/WearableManager.java
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/wearable/WearableManager.java
@@ -2,6 +2,7 @@ package com.eleks.securedatastorage.sdk.wearable;
 
 import android.content.Context;
 
+import com.eleks.securedatastorage.sdk.dialogs.PasswordDialog;
 import com.eleks.securedatastorage.sdk.interfaces.OnGetHalfOfKeyListener;
 import com.eleks.securedatastorage.sdk.interfaces.WearableSecureDataInterface;
 
@@ -12,12 +13,19 @@ public class WearableManager {
 
     private final Context mContext;
     private final WearableSecureDataInterface mWearableInterface;
-    private final OnGetHalfOfKeyListener mGetHalfOfKeyListener;
+    private OnGetHalfOfKeyListener mGetHalfOfKeyListener;
 
-    public WearableManager(Context context, WearableSecureDataInterface wearableInterface,
-                           OnGetHalfOfKeyListener getHalfOfKeyListener) {
+    public WearableManager(Context context, WearableSecureDataInterface wearableInterface) {
         this.mContext = context;
         this.mWearableInterface = wearableInterface;
+    }
+
+    public void getDeviceHalfOfKey(OnGetHalfOfKeyListener getHalfOfKeyListener) {
         this.mGetHalfOfKeyListener = getHalfOfKeyListener;
+        if (mWearableInterface != null) {
+            mWearableInterface.getDeviceHalfOfKey(getHalfOfKeyListener);
+        } else {
+            new PasswordDialog(mContext).show(getHalfOfKeyListener);
+        }
     }
 }
