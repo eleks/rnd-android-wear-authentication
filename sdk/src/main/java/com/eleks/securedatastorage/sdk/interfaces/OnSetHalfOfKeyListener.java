c4a80a3d030fe882670e9baa5811fdebebb0c2cb
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-22 15:32:32 +0300

    Added interface.

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/interfaces/OnSetHalfOfKeyListener.java
@@ -0,0 +1,8 @@
+package com.eleks.securedatastorage.sdk.interfaces;
+
+/**
+ * Created by Serhiy.Krasovskyy on 22.06.2015.
+ */
+public interface OnSetHalfOfKeyListener {
+    void OnSetHalfOfKey(int result);
+}
