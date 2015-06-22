04f73fdf2df670132ce7cd7a8c71d112adf1dfb8
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-22 21:51:43 +0300

    Added phone id as activity parameter.

--- wear/src/main/java/com/eleks/securedatastorage/activity/MainActivity.java
+++ wear/src/main/java/com/eleks/securedatastorage/activity/MainActivity.java
@@ -7,19 +7,23 @@ import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.eleks.securedatastorage.R;
+import com.eleks.securedatastorage.utils.Constants;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends Activity {
 
     private GoogleApiClient mClient;
+    private String mPhoneId;
 
 
-    public static void start(Context context) {
+    public static void start(Context context, String phoneId) {
         Intent startIntent = new Intent(context, MainActivity.class);
         startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startIntent.putExtra(Constants.Extras.PHONE_ID, phoneId);
         context.startActivity(startIntent);
     }
 
@@ -27,12 +31,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getExtras();
         initApi();
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 Button okButton = (Button) stub.findViewById(R.id.ok_button);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        pairDevices();
+                        finish();
+                    }
+                });
                 Button cancelButton = (Button) stub.findViewById(R.id.cancel_button);
                 cancelButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -42,6 +54,10 @@ public class MainActivity extends Activity {
                 });
             }
         });
+    }
+
+    private void pairDevices() {
+        Toast.makeText(this, "Phone Id = " + mPhoneId, Toast.LENGTH_LONG).show();
     }
 
     private void initApi() {
@@ -54,4 +70,7 @@ public class MainActivity extends Activity {
                 .build();
     }
 
+    public void getExtras() {
+        mPhoneId = getIntent().getExtras().getString(Constants.Extras.PHONE_ID, null);
+    }
 }
---
+++ wear/src/main/java/com/eleks/securedatastorage/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.eleks.securedatastorage.utils;
+
+/**
+ * Created by Sergey on 22.06.2015.
+ */
+public class Constants {
+
+    public static class Extras {
+        public static final String PHONE_ID = "phone_id";
+    }
+
+    public static class Security {
+        public static final String KEY_FILE_NAME = "device_key.dat";
+    }
+}
