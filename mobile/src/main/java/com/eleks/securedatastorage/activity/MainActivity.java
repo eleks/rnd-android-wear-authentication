2201bcbbb27a2b898a138a4eb5721e4505953c4d
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-22 17:58:17 +0300

    Added Test button.

--- mobile/src/main/java/com/eleks/securedatastorage/activity/MainActivity.java
+++ mobile/src/main/java/com/eleks/securedatastorage/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.eleks.securedatastorage.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.eleks.securedatastorage.R;
+import com.eleks.securedatastorage.sdk.gwatch.GWatchMessages;
+import com.eleks.securedatastorage.sdk.gwatch.GWatchMethods;
 import com.eleks.securedatastorage.sdk.storage.SecureStorageManager;
 import com.eleks.securedatastorage.utils.Constants;
 
@@ -50,6 +53,14 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
+
+        Button testButton = (Button) findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new SendMessageTask().execute();
+            }
+        });
     }
 
     private boolean isPasswordValid(String password) {
@@ -60,6 +71,16 @@ public class MainActivity extends ActionBarActivity {
     private boolean isUserNameValid(String userName) {
         //TODO Need to implement
         return true;
+    }
+
+    private class SendMessageTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            GWatchMethods gWatchMethods = new GWatchMethods(MainActivity.this);
+            gWatchMethods.sendMessage("cbd903a", GWatchMessages.Requests.SHOULD_USE_THIS_DEVICE_FOR_SECURE_STORAGE, null);
+            return null;
+        }
     }
 
 }
