8f6c9ea8e95e51fbb77212e30f5f6a861830b0ce
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-23 18:01:05 +0300

    Changed test according changes in class.

--- sdk/src/androidTest/java/com/eleks/securedatastorage/sdk/androidwatch/AndroidWatchMethods_Test.java
+++ sdk/src/androidTest/java/com/eleks/securedatastorage/sdk/androidwatch/AndroidWatchMethods_Test.java
@@ -2,14 +2,25 @@ package com.eleks.securedatastorage.sdk.androidwatch;
 
 import android.test.InstrumentationTestCase;
 
+import com.eleks.securedatastorage.sdk.interfaces.OnGetPairedDevice;
+
 /**
  * Created by Serhiy.Krasovskyy on 22.06.2015.
  */
 public class AndroidWatchMethods_Test extends InstrumentationTestCase {
 
     public void test_checkIfWearableConnected() {
-        AndroidWatchMethods androidWatchMethods = new AndroidWatchMethods(getInstrumentation().getTargetContext());
-        boolean result = androidWatchMethods.checkIfWearableConnected("49db30f6");
-        assertTrue(result);
+        AndroidWatchSecureData androidWatchSecureData=new AndroidWatchSecureData(getInstrumentation().getTargetContext());
+        androidWatchSecureData.isPairedDeviceConnected("49db30f6", new OnGetPairedDevice() {
+            @Override
+            public void getPairedDevice(boolean result) {
+                assertTrue(result);
+            }
+
+            @Override
+            public void getError(String errorMessage) {
+
+            }
+        });
     }
 }
