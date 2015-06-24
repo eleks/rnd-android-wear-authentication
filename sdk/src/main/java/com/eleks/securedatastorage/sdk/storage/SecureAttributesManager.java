a551349ea1c8d7bf7d5e75266f3cb6c0efaf6446
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-24 17:50:34 +0300

    Added class for store and load security attributes.

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/storage/SecureAttributesManager.java
@@ -0,0 +1,40 @@
+package com.eleks.securedatastorage.sdk.storage;
+
+import android.content.Context;
+
+import com.eleks.securedatastorage.sdk.model.SecureAttributes;
+import com.eleks.securedatastorage.sdk.utils.Constants;
+import com.eleks.securedatastorage.sdk.utils.IOHelper;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Serhiy.Krasovskyy on 24.06.2015.
+ */
+public class SecureAttributesManager {
+
+    public static void storeSecureAttributes(Context context, SecureAttributes secureAttributes)
+            throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(secureAttributes);
+        File secureAttributesFile =
+                new File(context.getFilesDir(), Constants.Security.ATTRIBUTES_FILE_NAME);
+        IOHelper.writeFileSources(secureAttributesFile, json);
+    }
+
+    public static SecureAttributes loadSecureAttributes(Context context)
+            throws IOException, JsonSyntaxException {
+        File secureAttributesFile =
+                new File(context.getFilesDir(), Constants.Security.ATTRIBUTES_FILE_NAME);
+        SecureAttributes result = null;
+        if (secureAttributesFile.exists()) {
+            String json = IOHelper.getFileSources(secureAttributesFile);
+            Gson gson = new Gson();
+            result = gson.fromJson(json, SecureAttributes.class);
+        }
+        return result;
+    }
+}
