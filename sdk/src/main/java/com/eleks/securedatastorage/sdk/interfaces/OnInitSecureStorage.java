7caa312f2352fb5201ef0e410b2912eb8bdaae5e
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-25 17:40:27 +0300

    Added new interface to support initialization of secure storage.

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/interfaces/OnInitSecureStorage.java
@@ -0,0 +1,8 @@
+package com.eleks.securedatastorage.sdk.interfaces;
+
+/**
+ * Created by Serhiy.Krasovskyy on 25.06.2015.
+ */
+public interface OnInitSecureStorage extends OnGetError {
+    void initSecureStorageSuccessfully();
+}
