ef559ee3fa1e8a6456f74b97b76044f3a781fef1
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-25 22:21:04 +0300

    Added interface for store data.

---
+++ sdk/src/main/java/com/eleks/securedatastorage/sdk/interfaces/OnStoreData.java
@@ -0,0 +1,8 @@
+package com.eleks.securedatastorage.sdk.interfaces;
+
+/**
+ * Created by Sergey on 25.06.2015.
+ */
+public interface OnStoreData extends OnGetError {
+    void dataStoredSuccessfully();
+}
