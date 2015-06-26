6b1bbf858dbad95b554008f8f96c018b157fb100
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-26 15:11:40 +0300

    Added initialize activity.

---
+++ mobile/src/main/java/com/eleks/securedatastorage/activity/InitializeActivity.java
@@ -0,0 +1,18 @@
+package com.eleks.securedatastorage.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.eleks.securedatastorage.R;
+
+/**
+ * Created by Serhiy.Krasovskyy on 26.06.2015.
+ */
+public class InitializeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initialize);
+    }
+}
