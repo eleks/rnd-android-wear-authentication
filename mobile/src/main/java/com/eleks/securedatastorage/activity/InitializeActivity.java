d9c97d1f6e435d8b41920dda5dc94529521d978a
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-26 17:13:03 +0300

    Added init controls method.

--- mobile/src/main/java/com/eleks/securedatastorage/activity/InitializeActivity.java
+++ mobile/src/main/java/com/eleks/securedatastorage/activity/InitializeActivity.java
@@ -2,6 +2,7 @@ package com.eleks.securedatastorage.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
 
 import com.eleks.securedatastorage.R;
 
@@ -10,9 +11,22 @@ import com.eleks.securedatastorage.R;
  */
 public class InitializeActivity extends ActionBarActivity {
 
+    private EditText mCardNumber;
+    private EditText mExpirationMonth;
+    private EditText mExpirationYear;
+    private EditText mCardCvv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initialize);
+        initControls();
+    }
+
+    private void initControls() {
+        mCardNumber=(EditText) findViewById(R.id.card_number);
+        mExpirationMonth=(EditText) findViewById(R.id.expiration_month);
+        mExpirationYear=(EditText) findViewById(R.id.expiration_year);
+        mCardCvv=(EditText)findViewById(R.id.card_cvv);
     }
 }
