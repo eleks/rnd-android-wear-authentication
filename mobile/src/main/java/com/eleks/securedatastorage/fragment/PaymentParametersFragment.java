20c154b1d153eb9ebe13c38978112b81e993b4bf
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-30 16:51:07 +0300

    Set values into controls.

--- mobile/src/main/java/com/eleks/securedatastorage/fragment/PaymentParametersFragment.java
+++ mobile/src/main/java/com/eleks/securedatastorage/fragment/PaymentParametersFragment.java
@@ -2,12 +2,14 @@ package com.eleks.securedatastorage.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.eleks.securedatastorage.R;
+import com.eleks.securedatastorage.utils.Constants;
 
 /**
  * Created by Serhiy.Krasovskyy on 30.06.2015.
@@ -20,41 +22,41 @@ public class PaymentParametersFragment extends Fragment {
     private EditText mExpirationMonth;
     private EditText mExpirationYear;
     private EditText mCardCvv;
+    private String mCardNumberValue;
+    private String mExpirationMonthValue;
+    private String mExpirationYearValue;
+    private String mCardCvvValue;
 
     public static PaymentParametersFragment getInstance() {
         return new PaymentParametersFragment();
+    }
+
+    public static PaymentParametersFragment getInstance(String cardNumber, String expirationMonth,
+                                                        String expirationYear, String cardCvv) {
+        PaymentParametersFragment fragment = new PaymentParametersFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.PaymentParameters.CARD_NUMBER, cardNumber);
+        bundle.putString(Constants.PaymentParameters.EXPIRATION_MONTH, expirationMonth);
+        bundle.putString(Constants.PaymentParameters.EXPIRATION_YEAR, expirationYear);
+        bundle.putString(Constants.PaymentParameters.CARD_CVV, cardCvv);
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
     public String getCardNumber() {
         return mCardNumber.getText().toString();
     }
 
-    public void setCardNumber(String cardNumber) {
-        this.mCardNumber.setText(cardNumber);
-    }
-
     public String getExpirationMonth() {
         return mExpirationMonth.getText().toString();
-    }
-
-    public void setExpirationMonth(String expirationMonth) {
-        this.mExpirationMonth.setText(expirationMonth);
     }
 
     public String getExpirationYear() {
         return mExpirationYear.getText().toString();
     }
 
-    public void setExpirationYear(String expirationYear) {
-        this.mExpirationYear.setText(expirationYear);
-    }
-
     public String getCardCvv() {
         return mCardCvv.getText().toString();
-    }
-
-    public void setCardCvv(String cardCvv) {
-        this.mCardCvv.setText(cardCvv);
     }
 
     @Override
@@ -67,8 +69,37 @@ public class PaymentParametersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mFragmentView = inflater.inflate(R.layout.fragment_payment_parameters, container, false);
+        getExtras();
         initFragmentControls();
+        fillFragmentControls();
         return mFragmentView;
+    }
+
+    private void fillFragmentControls() {
+        setEditTextValue(mCardNumber, mCardNumberValue);
+        setEditTextValue(mExpirationMonth, mExpirationMonthValue);
+        setEditTextValue(mExpirationYear, mExpirationYearValue);
+        setEditTextValue(mCardCvv, mCardCvvValue);
+    }
+
+    private void setEditTextValue(EditText editText, String value) {
+        if (!TextUtils.isEmpty(value)) {
+            editText.setText(value);
+            editText.setEnabled(false);
+            editText.setClickable(false);
+        }
+    }
+
+    private void getExtras() {
+        if (getArguments() != null) {
+            mCardNumberValue = getArguments()
+                    .getString(Constants.PaymentParameters.CARD_NUMBER, null);
+            mExpirationMonthValue = getArguments()
+                    .getString(Constants.PaymentParameters.EXPIRATION_MONTH, null);
+            mExpirationYearValue = getArguments()
+                    .getString(Constants.PaymentParameters.EXPIRATION_YEAR, null);
+            mCardCvvValue = getArguments().getString(Constants.PaymentParameters.CARD_CVV, null);
+        }
     }
 
     private void initFragmentControls() {
