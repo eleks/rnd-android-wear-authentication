72285de168730f7355efbdd66840d2b027ec25bc
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-30 16:41:01 +0300

    Added class to store payment parameter.

---
+++ mobile/src/main/java/com/eleks/securedatastorage/model/ParameterHolder.java
@@ -0,0 +1,13 @@
+package com.eleks.securedatastorage.model;
+
+/**
+ * Created by Serhiy.Krasovskyy on 30.06.2015.
+ */
+public class ParameterHolder {
+    public String parameterName;
+    public String parameterValue;
+
+    public ParameterHolder(String parameterName) {
+        this.parameterName = parameterName;
+    }
+}
