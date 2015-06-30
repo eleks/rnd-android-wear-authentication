5a132c5ddbbf50fe600d1e389e7b3132e6ad9cc3
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-30 10:31:30 +0300

    Added BaseActivity class as parent for all application activities.

--- mobile/src/main/java/com/eleks/securedatastorage/activity/BaseActivity.java
+++ mobile/src/main/java/com/eleks/securedatastorage/activity/BaseActivity.java
@@ -1,7 +1,37 @@
 package com.eleks.securedatastorage.activity;
 
+import android.app.ProgressDialog;
+import android.support.v7.app.ActionBarActivity;
+
+import com.eleks.securedatastorage.R;
+
 /**
  * Created by Serhiy.Krasovskyy on 30.06.2015.
  */
-public class BaseActivity {
+public class BaseActivity extends ActionBarActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    protected void showProgressDialog(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mProgressDialog = ProgressDialog.show(BaseActivity.this,
+                        BaseActivity.this.getString(R.string.app_name),
+                        message);
+            }
+        });
+    }
+
+    protected void dismissProgressDialog() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mProgressDialog != null) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        });
+    }
 }
