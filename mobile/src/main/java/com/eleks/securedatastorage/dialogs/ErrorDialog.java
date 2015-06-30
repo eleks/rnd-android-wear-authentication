5061799a19f50a33dea01f27128b99139019a5a8
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-30 10:16:03 +0300

    Added error dialog.

---
+++ mobile/src/main/java/com/eleks/securedatastorage/dialogs/ErrorDialog.java
@@ -0,0 +1,34 @@
+package com.eleks.securedatastorage.dialogs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.eleks.securedatastorage.R;
+
+/**
+ * Created by Serhiy.Krasovskyy on 30.06.2015.
+ */
+public class ErrorDialog {
+    private final AlertDialog.Builder mAlertDialog;
+    private final Context mContext;
+
+    public ErrorDialog(Context context, String errorMessage) {
+        this.mContext = context;
+        mAlertDialog = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.app_name))
+                .setMessage(errorMessage)
+                .setCancelable(true)
+                .setPositiveButton(context.getString(R.string.error_dialog_positive_answer),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.dismiss();
+                            }
+                        });
+    }
+
+    public void show() {
+        mAlertDialog.create().show();
+    }
+}
